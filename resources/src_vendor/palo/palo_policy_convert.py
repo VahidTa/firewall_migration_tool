6a1b34722bbe68371c8f2560763447d09b9bd315
Michael Kaufmann <michi.kaufmann1012@gmail.com>
2024-07-25 19:48:52 +0200

    Fixed problems with "Any" for dst Checkpoint. Checkpoint needs Any to be written with an Uppercase A at the beginning.
    Also palo alto's "application-default" option does not exist on checkpoint. Added translation to "Any"

--- resources/src_vendor/palo/palo_policy_convert.py
+++ resources/src_vendor/palo/palo_policy_convert.py
@@ -202,7 +202,7 @@ def palo_policy(file: str, vendor: str):
                     dst_list.append(f'destination.{i+1} "{policy_dst_list[i]}"')
                 policy_dst_address = ' '.join(dst_list)
             else:
-                policy_dst_address = f'destination "{policy_dst_address}"'
+                policy_dst_address = f'destination "{"Any" if str(policy_dst_address).strip().lower() == "any" else policy_dst_address}"' #checkpoint need to have "Any" as string not "any" (Case is important)
             
             if policy_src_list:
                 src_list = []
@@ -210,7 +210,7 @@ def palo_policy(file: str, vendor: str):
                     src_list.append(f'source.{i+1} "{policy_src_list[i]}"')
                 policy_src_address = ' '.join(src_list)
             else:
-                policy_src_address = f'source "{policy_src_address}"'
+                policy_src_address = f'source "{"Any" if str(policy_src_address).strip().lower() == "any" else  policy_src_address}"' #checkpoint need to have "Any" as string not "any" (Case is important)
             if policy_app_list:
                 app_list = []
                 for i in range(len(policy_app_list)):
@@ -223,7 +223,12 @@ def palo_policy(file: str, vendor: str):
             else:
                 if policy_app[:1].isdigit():
                     policy_app = 'custom_' + policy_app
-                policy_app = f'service "{policy_app}"'
+                elif str(policy_app).strip().lower() == "any" :
+                    policy_app = "Any" #checkpoint need to have "Any" as string not "any" (Case is important)
+                elif str(policy_app).strip().lower() == "application-default":
+                    policy_app = "Any" #application-default concept does not exist in checkpoint (see https://knowledgebase.paloaltonetworks.com/KCSArticleDetail?id=kA10g000000ClVwCAK)
+                policy_app = f'service {policy_app}'
+                
             
             chpoint = CHPoint_DST()
             chpoint.policy(
